b0320242204d4167ebcc7a6d0040f65ffed43cbe
pavillalta <Estudiante@DESKTOP-J6D5H3A>
2023-03-16 14:44:34 -0600

    terminacion del crud con PHP + MYSQL + Vue.js + Bootstrap + IndexedDB

--- private/modulos/docentes/docentes.php
+++ private/modulos/docentes/docentes.php
@@ -4,7 +4,12 @@ extract($_REQUEST);
 
 $docentes = isset($docentes) ? $docentes : '[]';
 $accion = isset($accion) ? $accion : '';
-
+$class_docente = new Docente($conexion);
+if( $accion=='consultar' ){
+    print_r(json_encode($class_docente->consultar('')));
+}else{
+    print_r($class_docente->recibir_datos($docentes));
+}
 class Docente{
     private $datos=[], $db, $respuesta=['msg'=>'ok'];
 
@@ -31,13 +36,28 @@ class Docente{
         global $accion;
         if( $this->respuesta['msg']=='ok' ){
             if($accion=='nuevo'){
-                
+                return $this->db->consultas(
+                    'INSERT INTO docentes VALUES(?,?,?)',
+                    $this->datos['idDocente'], $this->datos['codigo'], $this->datos['nombre']
+                );
             }else if($accion=='modificar'){
-
+                return $this->db->consultas(
+                    'UPDATE docentes SET codigo=?, nombre=? WHERE idDocente=?',
+                    $this->datos['codigo'], $this->datos['nombre'], $this->datos['idDocente']
+                );
+            }else if($accion=='eliminar'){
+                return $this->db->consultas(
+                    'DELETE docentes FROM docentes WHERE idDocente=?',
+                    $this->datos['idDocente']
+                );
             }
         }else{
             return $this->respuesta;
         }
+    }
+    public function consultar(){
+        $this->db->consultas('SELECT * FROM docentes');
+        return $this->db->obtener_datos();
     }
 }
 ?>
